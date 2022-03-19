e021f00002ee732bcc51ff7fd0370e09f331f14c
mdekauwe <mdekauwe@gmail.com>
2022-03-19 10:56:29 +0000

    added plot

--- calc_repayment.py
+++ calc_repayment.py
@@ -10,6 +10,9 @@ def get_repayments(principal, n_months, interest_rate):
 
     return (m)
 
+def find_nearest(array, value):
+    idx = (np.abs(array - value)).argmin()
+    return idx
 
 principal = float(sys.argv[1])
 n_years = 26
@@ -28,7 +31,15 @@ for i in range(len(interest_rates)):
     interest_rate = interest_rates[i] / 100 / 12.
     repayments_per_month[i] = get_repayments(principal, n_months, interest_rate)
 
-plt.plot(interest_rates, repayments_per_month, ls="--", marker="o")
+plt.plot(interest_rates, repayments_per_month, ls="-", color="black")
+
+for rt in [2.09, 3.0, 4.0, 5.0, 6.0, 7.0]:
+    id = find_nearest(interest_rates, rt)
+    plt.vlines(rt, interest_rates[0]-1.0, repayments_per_month[id], linestyle="dashed")
+    plt.hlines(repayments_per_month[id], interest_rates[0], rt, linestyle="dashed")
+
+
 plt.ylabel("Monthly repayment (\xA3)")
 plt.xlabel("Interest rate (%)")
+plt.ylim(repayments_per_month[0], repayments_per_month[-1]+300)
 plt.show()
